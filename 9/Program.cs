d5be4a1372024587fc45ffa6fb46624dde7f8a50
Baener <92623922+Baener@users.noreply.github.com>
2021-11-19 16:14:32 +0300

    Add files via upload

---
+++ 9/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m;
+            Console.WriteLine("Введите три числа, чтобы определить меньшее из них:");
+
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+            int z = Convert.ToInt32(Console.ReadLine());
+
+            m = x < y ? x < z ? x : z : y < z ? y : z;
+            Console.WriteLine($"min = {m}");
+        }
+    }
+}
