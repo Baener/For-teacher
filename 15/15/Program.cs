f12939373001ce49c853b1fb9c1ca6664a7b4938
Baener <92623922+Baener@users.noreply.github.com>
2021-11-27 17:12:29 +0300

    Add files via upload

---
+++ 15/15/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var mas = Array.ConvertAll(Console.ReadLine().Split(" "), s => int.Parse(s));
+            int min = mas[0];
+            int k = 0;
+
+            for (int i = 0; i < mas.Length; i++)
+            {
+                if (mas[i] < min)
+                {
+                    min = mas[i];
+                    k = i;
+                }
+            }
+            Console.WriteLine($"min = {min} and namber element = {k}");
+        }
+    }
+}
