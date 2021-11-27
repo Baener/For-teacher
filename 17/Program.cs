cd250dbe639ab23d31df883dedb42dc3693174b8
Baener <92623922+Baener@users.noreply.github.com>
2021-11-27 17:14:05 +0300

    Add files via upload

---
+++ 17/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var mas = Array.ConvertAll(Console.ReadLine().Split(" "), s => int.Parse(s));
+
+            int sum = 0;
+            for (int i = 0; i < mas.Length; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    sum += mas[i];
+                }
+            }
+            for (int i = 0; i < mas.Length; i++)
+            {
+                Console.Write(mas[i] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Сумма {sum}");
+        }
+    }
+}
