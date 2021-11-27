f37aa983acce1c13d3037e51fa15e3a8e3e042ae
Baener <92623922+Baener@users.noreply.github.com>
2021-11-27 17:14:50 +0300

    Add files via upload

---
+++ 18/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 8 чисел");
+            var mas = Array.ConvertAll(Console.ReadLine().Split(" "), s => int.Parse(s));
+            for (int i = 0; i < 8; i++)
+            {
+                if (mas[i] < 15)
+                {
+                    mas[i] *= mas[i];
+                }
+            }
+            for (int i = 0; i < 8; i++)
+            {
+                Console.Write(mas[i] + " ");
+            }
+        }
+    }
+}
